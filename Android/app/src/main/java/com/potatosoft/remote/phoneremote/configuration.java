cd4ae6a3cfdde96c2e1822eaebbe4f0b7b443701
sm_d3po <seanmarek@gmail.com>
2017-08-27 12:14:53 -0700

    android project commit

---
+++ Android/app/src/main/java/com/potatosoft/remote/phoneremote/configuration.java
@@ -0,0 +1,15 @@
+package com.potatosoft.remote.phoneremote;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class configuration extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuration);
+    }
+
+    
+}
