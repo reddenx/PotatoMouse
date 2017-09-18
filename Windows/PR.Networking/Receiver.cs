61937e2854992cf7e6064d88fb342f746a72adb2
sm_d3po <seanmarek@gmail.com>
2017-09-17 22:03:49 -0700

    application network interfaces written

--- Windows/PR.Networking/Receiver.cs
+++ Windows/PR.Networking/Receiver.cs
@@ -2,14 +2,20 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PR.Networking
 {
+    /// <summary>
+    /// abstracts message protocol from application layer
+    /// </summary>
     public class Receiver
     {
+        public event EventHandler OnMouseSignalReceived;
+
         public event EventHandler<Point> OnMouseMove;
         public event EventHandler<Point> OnMouseSet;
 
@@ -26,6 +32,11 @@ namespace PR.Networking
         public event EventHandler OnScrollwheelClick;
         public event EventHandler<int> OnScrollwheelMove;
 
+        public event EventHandler<string> OnKeyboardKeyClick; //this should be unambiguously cleaned up
+        public event EventHandler<string> OnKeyboardKeyPress; //this should be unambiguously cleaned up
+        public event EventHandler<string> OnKeyboardKeyRelease; //this should be unambiguously cleaned up
+        public event EventHandler<string> OnKeyboardKeyString;
+
         public event EventHandler<Exception> OnError;
 
         private readonly int Port;
@@ -41,7 +52,7 @@ namespace PR.Networking
             Udp.OnMessageReceived += Udp_OnMessageReceived;
             Udp.OnMessageSent += Udp_OnMessageSent;
         }
-        
+
         public void Start()
         {
             IsListening = true;
@@ -56,44 +67,172 @@ namespace PR.Networking
 
         private void Udp_OnMessageSent(object sender, string e)
         {
-            throw new NotImplementedException();
-        }
-
-        private void Udp_OnMessageReceived(object sender, string e)
-        {
-            //parse message
+            //no need for this, shouldn't be sending data
+            OnError?.Invoke(this, new IOException($"the underlying udp client is sending data through the receiver, this can cause unexpexted behavior and should be avoided"));
         }
 
         private void Udp_OnError(object sender, Exception e)
         {
             OnError?.Invoke(sender, e);
         }
+
+        private void Udp_OnMessageReceived(object sender, string message)
+        {
+            //message format:     [COMMAND_ID:DATA:TARGET_NAME]
+
+
+
+            OnMouseSignalReceived?.Invoke(this, EventArgs.Empty);
+
+            //is message valid
+            if (!message.StartsWith("[") || !message.EndsWith("]"))//message isn't boxed properly
+            {
+                OnError?.Invoke(this, new IOException($"message wasn't boxed properly: {message}"));
+                return;
+            }
+
+            var commandParts = message.Trim('[', ']').Split(':');
+            if (commandParts.Length != 3)//message had too many parts
+            {
+                OnError?.Invoke(this, new IOException($"message is not internally formatted correctly, should be 3 parts: {message}"));
+                return;
+            }
+
+            var commandId = 0;
+            if (!int.TryParse(commandParts[0], out commandId))
+            {
+                OnError?.Invoke(this, new IOException($"invalid commandId string, must be an integer value, received {commandParts[0]}"));
+                return;
+            }
+
+            //route message
+            switch (commandId)
+            {
+                case PhoneRemoteCommands.MOUSE_POSITION_MOVE:
+                    {
+                        //format of x,y
+                        var dataParts = commandParts[1].Split(',');
+                        var x = 0;
+                        var y = 0;
+                        if (dataParts.Length != 2 || !int.TryParse(dataParts[0], out x) || !int.TryParse(dataParts[1], out y))
+                        {
+                            OnError?.Invoke(this, new IOException($"move command data invalid: {commandParts[1]} in message {message}"));
+                        }
+
+                        if (x == 0 && y == 0) //not an error, just not useful
+                            return;
+
+                        OnMouseMove?.Invoke(this, new Point(x, y));
+                        break;
+                    }
+                case PhoneRemoteCommands.MOUSE_POSITION_SET:
+                    {
+                        var dataParts = commandParts[1].Split(',');
+                        var x = 0;
+                        var y = 0;
+                        if (dataParts.Length != 2 || !int.TryParse(dataParts[0], out x) || !int.TryParse(dataParts[1], out y))
+                        {
+                            OnError?.Invoke(this, new IOException($"set command data invalid: {commandParts[1]} in message {message}"));
+                        }
+
+                        OnMouseSet?.Invoke(this, new Point(x, y));
+                        break;
+                    }
+                case PhoneRemoteCommands.MOUSE_LEFT_CLICK:
+                    OnMouseLeftClick?.Invoke(this, EventArgs.Empty);
+                    break;
+                case PhoneRemoteCommands.MOUSE_LEFT_DOUBLE_CLICK:
+                    OnMouseDoubleLeftClick?.Invoke(this, EventArgs.Empty);
+                    break;
+                case PhoneRemoteCommands.MOUSE_LEFT_PRESS:
+                    OnMouseLeftPress?.Invoke(this, EventArgs.Empty);
+                    break;
+                case PhoneRemoteCommands.MOUSE_LEFT_RELEASE:
+                    OnMouseLeftRelease?.Invoke(this, EventArgs.Empty);
+                    break;
+                case PhoneRemoteCommands.MOUSE_RIGHT_CLICK:
+                    OnMouseRightClick?.Invoke(this, EventArgs.Empty);
+                    break;
+                case PhoneRemoteCommands.MOUSE_RIGHT_DOUBLE_CLICK:
+                    OnMouseDoubleRightClick?.Invoke(this, EventArgs.Empty);
+                    break;
+                case PhoneRemoteCommands.MOUSE_RIGHT_PRESS:
+                    OnMouseRightPress?.Invoke(this, EventArgs.Empty);
+                    break;
+                case PhoneRemoteCommands.MOUSE_RIGHT_RELEASE:
+                    OnMouseRightRelease?.Invoke(this, EventArgs.Empty);
+                    break;
+                case PhoneRemoteCommands.MOUSE_SCROLLWHEEL_CLICK:
+                    OnScrollwheelClick?.Invoke(this, EventArgs.Empty);
+                    break;
+                case PhoneRemoteCommands.MOUSE_SCROLLWHEEL_SCROLL_MOVE:
+                    {
+                        var moveAmount = 0;
+                        if (!int.TryParse(commandParts[1], out moveAmount))
+                        {
+                            OnError?.Invoke(this, new IOException($"mouse scroll data invalid: {commandParts[1]}, message: {message}"));
+                            return;
+                        }
+
+                        OnScrollwheelMove?.Invoke(this, moveAmount);
+                        break;
+                    }
+                case PhoneRemoteCommands.KEYBOARD_KEY_CLICK:
+                    {
+                        if(string.IsNullOrWhiteSpace(commandParts[1]))
+                        {
+                            OnError?.Invoke(this, new IOException($"keyboard click data missing, message: {message}"));
+                            return;
+                        }
+
+                        OnKeyboardKeyClick?.Invoke(this, commandParts[1]);
+                        break;
+                    }
+                case PhoneRemoteCommands.KEYBOARD_KEY_PRESS:
+                    {
+                        if (string.IsNullOrWhiteSpace(commandParts[1]))
+                        {
+                            OnError?.Invoke(this, new IOException($"keyboard press data missing, message: {message}"));
+                            return;
+                        }
+
+                        OnKeyboardKeyPress?.Invoke(this, commandParts[1]);
+                        break;
+                    }
+                case PhoneRemoteCommands.KEYBOARD_KEY_RELEASE:
+                    {
+                        if (string.IsNullOrWhiteSpace(commandParts[1]))
+                        {
+                            OnError?.Invoke(this, new IOException($"keyboard release data missing, message: {message}"));
+                            return;
+                        }
+
+                        OnKeyboardKeyRelease?.Invoke(this, commandParts[1]);
+                        break;
+                    }
+                case PhoneRemoteCommands.KEYBOARD_KEY_STRING:
+                    {
+                        if (string.IsNullOrWhiteSpace(commandParts[1]))
+                        {
+                            OnError?.Invoke(this, new IOException($"keyboard string data missing, message: {message}"));
+                            return;
+                        }
+
+                        OnKeyboardKeyString?.Invoke(this, commandParts[1]);
+                        break;
+                    }
+                case PhoneRemoteCommands.INVALID_COMMAND:
+                default:
+                    {
+                        OnError?.Invoke(this, new IOException($"invalid commandId, no command associated with ID: {commandId}, message: {message}"));
+                        return;
+                    }
+            }
+        }
     }
 
 
 
-    internal static class PhoneRemoteCommands //protocol to adhere to
-    {
-        public const int MOUSE_POSITION_MOVE = 0;
-        public const int MOUSE_POSITION_SET = 1;
-
-        public const int MOUSE_LEFT_CLICK = 10;
-        public const int MOUSE_LEFT_DOUBLE_CLICK = 11;
-        public const int MOUSE_LEFT_PRESS = 12;
-        public const int MOUSE_LEFT_RELEASE = 13;
-
-        public const int MOUSE_RIGHT_CLICK = 20;
-        public const int MOUSE_RIGHT_DOUBLE_CLICK = 21;
-        public const int MOUSE_RIGHT_PRESS = 22;
-        public const int MOUSE_RIGHT_RELEASE = 23;
-
-        public const int MOUSE_SCROLLWHEEL_CLICK = 30;
-        public const int MOUSE_SCROLLWHEEL_SCROLL_MOVE = 31;
-
-        public const int KAYBOERD_KEY_CLICK = 40;
-        public const int KEYBOARD_KEY_PRESS = 41;
-        public const int KEYBOARD_KEY_RELEASE = 42;
-        public const int KEYBOARD_KEY_STRING = 43;
-    }
+   
 }
 
---
+++ Windows/PR.Networking/RemoteCommands.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR.Networking
+{
+    internal static class PhoneRemoteCommands //protocol to adhere to
+    {
+        public const int INVALID_COMMAND = 0;
+        public const int MOUSE_POSITION_MOVE = 1;
+        public const int MOUSE_POSITION_SET = 2;
+
+        public const int MOUSE_LEFT_CLICK = 10;
+        public const int MOUSE_LEFT_DOUBLE_CLICK = 11;
+        public const int MOUSE_LEFT_PRESS = 12;
+        public const int MOUSE_LEFT_RELEASE = 13;
+
+        public const int MOUSE_RIGHT_CLICK = 20;
+        public const int MOUSE_RIGHT_DOUBLE_CLICK = 21;
+        public const int MOUSE_RIGHT_PRESS = 22;
+        public const int MOUSE_RIGHT_RELEASE = 23;
+
+        public const int MOUSE_SCROLLWHEEL_CLICK = 30;
+        public const int MOUSE_SCROLLWHEEL_SCROLL_MOVE = 31;
+
+        public const int KEYBOARD_KEY_CLICK = 40;
+        public const int KEYBOARD_KEY_PRESS = 41;
+        public const int KEYBOARD_KEY_RELEASE = 42;
+        public const int KEYBOARD_KEY_STRING = 43;
+    }
+}
