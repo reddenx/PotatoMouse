1115ec6adef1c2f91de7554c52205a1c978a98a7
sm_d3po <seanmarek@gmail.com>
2022-08-21 17:48:47 -0700

    started work on qr scanner

---
+++ Web/client-app/src/App2.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    TEST:
+    <button @click="buttonPressed">CLICK ME</button>
+    <div style="padding: 1em; border: 1px solid black;">{{ output }}</div>
+    <canvas style="border: 1px solid black;" ref="test" v-show="showCamera"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    output: "",
+    showCamera: false,
+  }),
+  methods: {
+    buttonPressed() {
+        this.showCamera = true;
+        console.log(this.$refs.test);
+
+    },
+  },
+};
+</script>
+
+<style>
+</style>
