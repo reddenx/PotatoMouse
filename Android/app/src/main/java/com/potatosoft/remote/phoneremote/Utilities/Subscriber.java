a9ee4f45cbdb925144972e5f3dea1fd467aeadb9
sm_d3po <seanmarek@gmail.com>
2023-10-22 15:54:07 -0700

    repo cleanup

--- Android/app/src/main/java/com/potatosoft/remote/phoneremote/Utilities/Subscriber.java
+++
@@ -1,9 +0,0 @@
-package com.potatosoft.remote.phoneremote.Utilities;
-
-/**
- * Created by seanm_000 on 9/18/2017.
- */
-
-public abstract class Subscriber<T> {
-    public abstract void HandleMessage(T message);
-}
